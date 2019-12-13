85721996ea29dc2fd79397a316b7649a76aa76e9
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-12-13 12:36:15 -0500

    trying to get response from API

---
+++ smurfs/src/components/Smurfs.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { connect } from "react-redux";
+import { getSmurfs } from "../actions";
+
+const Smurfs = props => {
+  return (
+    <div>
+      <h1>Welcome to the Smurf Village!</h1>
+      {props.smurfs &&
+        props.smurfs.map(smurf => (
+          <div>
+            <h1>Name: {smurf.name}</h1>
+            <p>Age: {smurf.age}</p>
+            <p>Height: {smurf.height}</p>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+    return {
+        smurfs: state.smurfs,
+        fetchingSmurfs: state.fetchingSmurfs,
+        error: state.error
+    }
+}
+
+export default connect(mapStateToProps, { getSmurfs })(Smurfs);
